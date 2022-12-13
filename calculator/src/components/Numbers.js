056cda41148a875d9db85ecef914e96e38e9a229
Hanna Arefiardakani <darkshiirarhotmail.com>
2022-12-13 15:58:51 +0100

    calculator done

---
+++ calculator/src/components/Numbers.js
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import './Numbers.css';
+
+
+const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+const state = ["+", "-", "*", "/"];
+
+
+
+const Numbers = () => {
+    const [currentnumber, setCurrentNumber] = useState(numbers[9]);
+    const [aState, setAState] = useState(state[0]);
+    const [memory, setMemory] = useState(0);
+
+    const operatingFunction = (state) => {
+        setMemory(memory + currentnumber);
+        setCurrentNumber(0);
+        setAState(state);
+    }
+    
+    const viewState = () => {
+        if (aState === "+") {
+            setCurrentNumber(parseInt(memory) + currentnumber);
+        } else if (aState === "-") {
+            setCurrentNumber(parseInt(memory) - currentnumber);
+        } else if (aState === "*") {
+            setCurrentNumber(parseInt(memory) * currentnumber);
+        } else if (aState === "/") {
+            setCurrentNumber(parseInt(memory) / currentnumber);
+        }
+        return currentnumber
+    }
+
+  return (
+    <>
+    <div className="solutionBox">
+        <p>{currentnumber}</p>
+    </div>
+    <div className="operatingBox">
+        <div className= "numbersquare">
+        
+            {numbers.map((number, index) => 
+                <button key={index} onClick={() => setCurrentNumber(parseInt(currentnumber.toString() + number.toString()))}>{number}</button>
+                )}
+    <button onClick = {() => viewState()}> = </button>
+    <button onClick = {() => setCurrentNumber(0)}> C </button>
+         </div>
+    <div className="operatorsBox">
+    {state.map((state, index) =>
+        <button key={index} onClick={() => operatingFunction(state)}>{state}</button>)}
+    
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Numbers
